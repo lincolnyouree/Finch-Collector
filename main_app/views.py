71216b573143b62373b7824720b9de35cd17de3f
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-03-16 16:10:20 -0500

    Setup Admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Lolo', 'tabby', 'foul little demon', 3),
-  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Finch('Raven', 'black tripod', '3 legged cat', 4)
-]
+from .models import Finch
 
 # Create your views here.
 
@@ -23,4 +11,5 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
